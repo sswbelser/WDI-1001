446e23ccb3f93fe6cf2110253b8210990911e416
Sabastian <sswbelser@gmail.com>
2015-08-25 12:19:29 -0700

    Changed routes and controllers

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 var app = angular.module('starter', ['ngRoute', 'starter.controllers'])
 
-app.config(['$routeProvider',
-	function($routeProvider) {
-		$routeProvider.
-			when('/todo', {
+app.config(['$routeProvider', '$locationProvider',
+	function ($routeProvider, $locationProvider) {
+		$routeProvider
+			.when('/', {
 				templateUrl: 'templates/todos.html',
 				controller: 'TodoCtrl'
-			}).
-			otherwise({
+			})
+			.when('/about', {
+				templateUrl: 'templates/about.html'
+			})
+			.when('/lists', {
+				templateUrl: 'templates/lists.html'
+			})
+			.otherwise({
 				redirectTo: '/todo'
 			});
+
+		// use the HTML5 History API
+		$locationProvider.html5Mode(true);
 	}]);
--- controllers.js
+++ controllers.js
@@ -2,18 +2,34 @@ var app = angular.module('starter.controllers', [])
 
 app.controller("TodoCtrl", ['$scope', '$rootScope', function ($scope, $rootScope) {
 	$scope.todos = [
-		{title: "Scrub floor"},
-		{title: "Vacuum house"},
-		{title: "Eat dinner"}
+		{title: 'Practice angular', kind: 'Work', created_at: 1440392214138, completed: false},
+		{title: 'Buy real food', kind: 'Health', created_at: 1440291414138, completed: true},
+		{title: 'Hit the gym', kind: 'Health', created_at: 1440182814138, completed: false}
 	];
-	$scope.newTodo = {title: ""};
-	$scope.click = function () {
-		$scope.todos.push($scope.newTodo)
-		$scope.newTodo = {title: ""}
-	};
-	$scope.delete = function (todo) {
-		var index = $scope.todos.indexOf(todo);
-		$scope.todos.splice(index, 1);
-	};
-	$scope.createdAt = new Date();
+	$scope.kinds = ['Work', 'Health', 'Fun'];
+	$scope.newTodo = {};
+	$scope.createToDo = function (){
+		var newToDo = {
+			title: $scope.newFormData.title, 
+			kind: $scope.newFormData.newKind, 
+			created_at: Date.now()
+		};
+		$scope.todos.push(newToDo);
+		$scope.newFormData.title = "";
+	}
+	$scope.deleteToDo = function (todo){
+		var i = $scope.todos.indexOf(todo);
+		$scope.todos.splice(i,1);
+	}
+	$scope.completeToDo = function (todo){
+		if (todo.completed == true) {
+			todo.completed = false
+		} else if (todo.completed == false) {
+			todo.completed = true;
+		}
+	}
+}]);
+
+app.controller("ListCtrl", ['$scope', '$rootScope', function ($scope, $rootScope) {
+	console.log("Lists works");
 }]);
