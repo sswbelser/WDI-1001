ab056954978f445376d69bb3f4731ec749341822
Sabastian <sswbelser@gmail.com>
2015-08-25 09:35:13 -0700

    Set up routes and controllers

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-var app = angular.module('starter', [])
+var app = angular.module('starter', ['ngRoute', 'starter.controllers'])
 
-app.controller("TodoCtrl", ['$scope', '$rootScope', function ($scope, $rootScope) {
-	$scope.todos = [
-		{title: "Scrub floor"},
-		{title: "Vacuum house"},
-		{title: "Eat dinner"}
-	];
-	$scope.newTodo = {title: ""};
-	$scope.click = function () {
-		$scope.todos.push($scope.newTodo)
-		$scope.newTodo = {title: ""}
-	};
-	$scope.delete = function (todo) {
-		var index = $scope.todos.indexOf(todo);
-		$scope.todos.splice(index, 1);
-	};
-	$scope.createdAt = new Date();
-}])
+app.config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider.
+			when('/todo', {
+				templateUrl: 'templates/todos.html',
+				controller: 'TodoCtrl'
+			}).
+			otherwise({
+				redirectTo: '/todo'
+			});
+	}]);
